4a3fa1c7d55999df0bc87ccf52f8af8b4cb478e8
edigonzales <edi.gonzales@gmail.com>
2023-02-09 20:35:58 +0100

    wip

--- src/main/java/ch/interlis/ioxwkf/flatgeobuf/FlatGeobufWriter.java
+++ src/main/java/ch/interlis/ioxwkf/flatgeobuf/FlatGeobufWriter.java
@@ -4,18 +4,28 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.Map;
 
 import ch.ehi.basics.settings.Settings;
+import ch.interlis.ili2c.metamodel.TransferDescription;
 import ch.interlis.iom.IomObject;
 import ch.interlis.iox.IoxEvent;
 import ch.interlis.iox.IoxException;
 import ch.interlis.iox.IoxFactoryCollection;
 import ch.interlis.iox.IoxWriter;
+import ch.interlis.iox.ObjectEvent;
+import ch.interlis.iox.StartBasketEvent;
+import ch.interlis.iox.StartTransferEvent;
 
 public class FlatGeobufWriter implements IoxWriter {
 
     private OutputStream outputStream;
     
+//    private Map<String,AttributeDescriptor> attrDescsMap=null;
+    
+    private TransferDescription td=null;
+    private String iliGeomAttrName=null;
+
     public FlatGeobufWriter(File file) throws IoxException {
         this(file,null);
     }
@@ -29,6 +39,139 @@ public class FlatGeobufWriter implements IoxWriter {
             this.outputStream = new FileOutputStream(file);
         } catch (FileNotFoundException e) {
             throw new IoxException(e);
+        }
+    }
+
+    @Override
+    public void write(IoxEvent event) throws IoxException {
+        if(event instanceof StartTransferEvent){
+            // ignore
+        }else if(event instanceof StartBasketEvent){
+        }else if(event instanceof ObjectEvent){
+            ObjectEvent obj = (ObjectEvent) event;
+            IomObject iomObj = (IomObject)obj.getIomObject();
+            String tag = iomObj.getobjecttag();
+            System.out.println("tag: " + tag);
+
+            // Wenn null, dann gibt es noch kein "Schema"
+//            attrDescsMap
+            if(null == null) {
+               // initAttrDescs(); // TODO ? 
+                if(td != null) {
+                    // TODO
+                } else {
+                    for(int u=0;u<iomObj.getattrcount();u++) {
+                        String attrName = iomObj.getattrname(u);
+                        System.out.println(attrName);
+                        //create the builder
+//                        AttributeTypeBuilder attributeBuilder = new AttributeTypeBuilder();
+                        //if(attrName.equals(iliGeomAttrName)) {
+                        if(attrName.equals("gaga")) {
+//                            iliGeomAttrName=attrName;
+//                            IomObject iomGeom=iomObj.getattrobj(attrName,0);
+//                            if (iomGeom != null){
+//                                if (iomGeom.getobjecttag().equals(COORD)){
+//                                    attributeBuilder.setBinding(Point.class);
+//                                }else if (iomGeom.getobjecttag().equals(MULTICOORD)){
+//                                    attributeBuilder.setBinding(MultiPoint.class);
+//                                }else if(iomGeom.getobjecttag().equals(POLYLINE)){
+//                                    attributeBuilder.setBinding(LineString.class);
+//                                }else if (iomGeom.getobjecttag().equals(MULTIPOLYLINE)){
+//                                    attributeBuilder.setBinding(MultiLineString.class);
+//                                }else if (iomGeom.getobjecttag().equals(MULTISURFACE)){
+//                                    int surfaceCount=iomGeom.getattrvaluecount("surface");
+//                                    if(surfaceCount<=1) {
+//                                        /* Weil der Featuretype (das Schema) des Shapefiles anhand des ersten IomObjektes erstellt wird, 
+//                                         * kann es vorkommen, dass Multisurfaces mit mehr als einer Surface nicht zu einem Multipolygon umgewandelt werden, 
+//                                         * sondern zu einem Polygon. Aus diesem Grund wird immer das MultiPolygon-Binding verwendet. */
+//                                        attributeBuilder.setBinding(MultiPolygon.class);
+//                                    }else if(surfaceCount>1){
+//                                        attributeBuilder.setBinding(MultiPolygon.class);
+//                                    }
+//                                }else {
+//                                    attributeBuilder.setBinding(Point.class);
+//                                }
+//                                if(defaultSrsId!=null) {
+//                                    attributeBuilder.setCRS(createCrs(defaultSrsId));
+//                                }
+//                            }
+                        } else {
+                            // Ist das nicht relativ heikel?
+                            // Funktioniert mit Strukturen nicht mehr, oder? Wegen getattrvaluecount?
+                            // TODO: testen
+                            if(iliGeomAttrName == null && iomObj.getattrvaluecount(attrName)>0 && iomObj.getattrobj(attrName,0) != null) {
+                                iliGeomAttrName=attrName;
+                                System.out.println("geometry found");
+//                                IomObject iomGeom=iomObj.getattrobj(attrName,0);
+//                                if (iomGeom != null){
+//                                    if (iomGeom.getobjecttag().equals(COORD)){
+//                                        attributeBuilder.setBinding(Point.class);
+//                                    }else if (iomGeom.getobjecttag().equals(MULTICOORD)){
+//                                        attributeBuilder.setBinding(MultiPoint.class);
+//                                    }else if(iomGeom.getobjecttag().equals(POLYLINE)){
+//                                        attributeBuilder.setBinding(LineString.class);
+//                                    }else if (iomGeom.getobjecttag().equals(MULTIPOLYLINE)){
+//                                        attributeBuilder.setBinding(MultiLineString.class);
+//                                    }else if (iomGeom.getobjecttag().equals(MULTISURFACE)){
+//                                        int surfaceCount=iomGeom.getattrvaluecount("surface");
+//                                        if(surfaceCount==1) {
+//                                            /* Weil der Featuretype (das Schema) des Shapefiles anhand des ersten IomObjektes erstellt wird, 
+//                                             * kann es vorkommen, dass Multisurfaces mit mehr als einer Surface nicht zu einem Multipolygon umgewandelt werden, 
+//                                             * sondern zu einem Polygon. Aus diesem Grund wird immer das MultiPolygon-Binding verwendet. */
+//                                            attributeBuilder.setBinding(MultiPolygon.class);
+//                                        }else if(surfaceCount>1){
+//                                            attributeBuilder.setBinding(MultiPolygon.class);
+//                                        }
+//                                    }else {
+//                                        attributeBuilder.setBinding(Point.class);
+//                                    }
+//                                    if(defaultSrsId!=null) {
+//                                        attributeBuilder.setCRS(createCrs(defaultSrsId));
+//                                    }
+//                                }
+                            } else {
+                                //attributeBuilder.setBinding(String.class);
+                            }
+                        }
+//                        attributeBuilder.setName(attrName);
+//                        attributeBuilder.setMinOccurs(0);
+//                        attributeBuilder.setMaxOccurs(1);
+//                        attributeBuilder.setNillable(true);
+//                        //build the descriptor
+//                        String trimmedAttrName = trimAttributeName(attrName,9);
+//                        AttributeDescriptor descriptor = attributeBuilder.buildDescriptor(trimmedAttrName);                            
+//                        addAttrDesc(attrName, descriptor);  
+                    }
+                }
+            }
+//            if(featureType==null) {
+//                featureType=createFeatureType(attrDescs);
+//                featureBuilder = new SimpleFeatureBuilder(featureType);
+//                try {
+//                    dataStore.createSchema(featureType);
+//                    
+//                    String typeName = dataStore.getTypeNames()[0];
+//                    
+//                    transaction = new DefaultTransaction(
+//                            "create");
+//
+//                    writer = dataStore.getFeatureWriter(typeName, transaction);
+//                } catch (IOException e) {
+//                    throw new IoxException(e);
+//                }
+//            }
+//            // write object attribute-values of model attribute-names
+//            try {
+//                SimpleFeature feature=convertObject(iomObj);
+//
+//                writeFeatureToShapefile(feature);
+//            } catch (IOException e) {
+//                throw new IoxException("failed to write object "+iomObj.getobjecttag(),e);
+//            } catch (Iox2jtsException e) {
+//                throw new IoxException("failed to convert "+iomObj.getobjecttag()+" in jts",e);
+//            }
+ 
+            
         }
     }
 
@@ -63,10 +206,5 @@ public class FlatGeobufWriter implements IoxWriter {
 
     }
 
-    @Override
-    public void write(IoxEvent arg0) throws IoxException {
-        // TODO Auto-generated method stub
-
-    }
 
 }
---
+++ src/test/java/ch/interlis/ioxwkf/flatgeobuf/FlatGeobufWriterTest.java
@@ -0,0 +1,65 @@
+package ch.interlis.ioxwkf.flatgeobuf;
+
+import java.io.File;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ch.interlis.iom.IomObject;
+import ch.interlis.iom_j.Iom_jObject;
+import ch.interlis.iox.IoxException;
+import ch.interlis.iox_j.EndBasketEvent;
+import ch.interlis.iox_j.EndTransferEvent;
+import ch.interlis.iox_j.ObjectEvent;
+import ch.interlis.iox_j.StartBasketEvent;
+import ch.interlis.iox_j.StartTransferEvent;
+
+public class FlatGeobufWriterTest {
+    
+    private static final String TEST_IN="src/test/data/FlatGeobufWriter/";
+    private final static String TEST_OUT="build/test/data/FlatGeobufWriter";
+
+    @BeforeClass
+    public static void setupFolder()
+    {
+        new File(TEST_OUT).mkdirs();
+    }
+
+    @Test
+    public void foo() throws IoxException {
+        Iom_jObject inputObj = new Iom_jObject("Test1.Topic1.Point", "o1");
+        inputObj.setattrvalue("id1", "1");
+        inputObj.setattrvalue("Text", "text1");
+        inputObj.setattrvalue("Double", "53434");
+        IomObject coordValue = inputObj.addattrobj("attrPoint", "COORD");
+        coordValue.setattrvalue("C1", "2600000.000");
+        coordValue.setattrvalue("C2", "1200000.000");
+        FlatGeobufWriter writer = null;
+        File file = new File(TEST_OUT,"foo.fgb");
+        try {
+            writer = new FlatGeobufWriter(file);
+            writer.write(new StartTransferEvent());
+            writer.write(new StartBasketEvent("Test1.Topic1","bid1"));
+            writer.write(new ObjectEvent(inputObj));
+            writer.write(new EndBasketEvent());
+            writer.write(new EndTransferEvent());
+        } catch(IoxException e) {
+            throw new IoxException(e);
+        } finally {
+            if(writer != null) {
+                try {
+                    writer.close();
+                } catch (IoxException e) {
+                    throw new IoxException(e);
+                }
+                writer=null;
+            }
+        }
+        
+        
+        
+        
+        
+    }
+    
+}
